b5ba75ea7858ac9180e42e9df80446a09228587f
Joshua Levie <johndoe@example.com>
2022-10-13 14:09:15 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CSE_210_HIGHLOW.Game;
+
+
+namespace CSE_210_HIGHLOW
+{
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
+
+        /// Main(string[] args) allows for arguments
+        static int Main(string[] args)
+        {
+            Director director = new Director();
+            /// Need to find director class 
+            director.StartGame();
+            /// returns 0 because we begin with 'static int' as opposed to 'static void'
+            return 0;
+        }
+    }
+}
